e0ba077980aef57ca8eae143afc8968eeb4a1a7d
Besson Mikael <73292500+MikaelBesson@users.noreply.github.com>
2020-12-06 01:46:03 +0100

    ajout du bouton suprimer l'ingredient

--- exo 1/script.js
+++ exo 1/script.js
@@ -9,18 +9,16 @@ buttonEnvoyer.addEventListener('click', function (event){
     let liste1 = document.getElementById("liste-commissions");
     let item = document.createElement("li");
     item.append(valueajoutez);
-    function recette(){
-        liste1.append(item);
-    }
-    recette();
-
+    liste1.append(item);
 })
 
 let buttonretirer = document.getElementById('suprimer');
-buttonretirer.removeEventListener('click', function (event){
+buttonretirer.addEventListener('click', function (event){
    event.preventDefault();
-
-
+   let liste = document.getElementsByTagName('li');
+   let listeNb =liste.length;
+   let dernierLi = liste[listeNb - 1];
+   dernierLi.remove();
 })
 
 
